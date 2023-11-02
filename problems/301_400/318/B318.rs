57ef348b8037fed36495972e4ed8505607b6bd81
Brandon H <Brandon@brandon-huu.com>
2023-11-02 13:13:23 -0500

    B318

--- problems/301_400/318/B318.rs
+++ problems/301_400/318/B318.rs
@@ -1,8 +1,15 @@
 /*
-Not complete.
-Have not even tried to submit this file.
-Can't run single rust files so uuuuuuuuuuuuuuuuuh
+Name: Strings of Power
+Url: https://codeforces.com/problemset/problem/318/B
+Completed: November2, 2023
+
+Initially I wanted a trie implementation I could incrementally advance down so i wont have to waste operations.
+
+However,
+I already had a solution that worked.
+and I'm on a time limit so i submitted what I knew what worked and not what was best
 */
+
 use std::collections::HashMap;
 
 #[derive(Default, Debug)]
@@ -18,30 +25,36 @@ pub struct Trie {
 
 impl Trie {
     pub fn new() -> Self {
-        Trie { root: TrieNode::default() }
+        Trie {
+            root: TrieNode::default(),
+        }
     }
-    pub fn from(words: &[&str]) -> self {
-        let mut word_bank: Trie { root:TrieNode::default() };
-        words.iter().for_each(word_bank.insert);
+    pub fn from(words: &[&str]) -> Self {
+        let mut word_bank = Trie {
+            root: TrieNode::default(),
+        };
+        words.iter().for_each(|word| word_bank.insert(word));
+        word_bank
     }
 
     pub fn insert(&mut self, word: &str) {
         word.chars()
-            .fold(&mut self.root, |current_node, letter| current_node.children.entry(c).or_default());
+            .fold(&mut self.root, |current_node, letter| {
+                current_node.children.entry(letter).or_default()
+            })
+            .is_end_of_word = true;
     }
 
     pub fn contains(&self, word: &str) -> Option<bool> {
-
-        match words.chars()
-            .fold(&self.root, |current_node, letter| current_node.children.get(&letter)) 
-            {
-                None => None,
-                Some(word) => Some(word.is_end_of_word)
-            }
-    }       
- 
+        match word.chars().try_fold(&self.root, |current_node, letter| {
+            current_node.children.get(&letter)
+        }) {
+            None => None,
+            Some(word) => Some(word.is_end_of_word),
+        }
+    }
 }
--+  
+
 #[derive(Default)]
 struct Scanner {
     buffer: Vec<String>,
@@ -65,15 +78,33 @@ impl Scanner {
 fn main() {
     let mut scanner = Scanner::default();
 
-    let word:String = scanner.next();
+    let word: String = scanner.next();
 
-    let mut metals:usize = 0;
-    let mut powerful_substrings:usize = 0;
+    let mut metals: usize = 0;
+    let mut powerful_substrings: usize = 0;
+    let mut word_bank: Trie = Trie::from(&["heavy", "metal"]);
+
     let mut current_word = String::new();
-    let mut word_bank: Trie = Trie::new();
-    word_bank
-    for letter in word.chars(){
-        match 
+
+    for letter in word.chars() {
+        current_word.push(letter);
+
+        match (
+            word_bank.contains(&current_word.as_str()),
+            current_word.as_str(),
+        ) {
+            (Some(true), "heavy") => {
+                metals += 1;
+                current_word = String::new();
+            }
+            (Some(true), "metal") => {
+                powerful_substrings += metals;
+                current_word = String::new();
+            },
+            (None, _) => current_word = letter.to_string(),
+            _ => (),
+        }
     }
-    println!("{}",powerful_substrings);
+
+    println!("{}", powerful_substrings);
 }
